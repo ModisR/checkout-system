abee6d5da7e6057f80dfb9318e7d01d72bf4b1f0
Modestas Rukšnaitis <modestas+github@ruksnaitis.com>
2022-11-14 15:53:03 +0000

    implemented shopping cart

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -1,5 +1,21 @@
 package uk.softar
 
+import scala.io.StdIn.readLine
+
 object CheckoutSystem extends App {
-  println("Hello, Mercator Digital!")
+  private val prices = Map[String, BigDecimal](
+    "apple" ->  .60,
+    "orange" -> .25
+  )
+
+  println("Please enter a space-separated list of products.")
+
+  val totalCost = readLine
+    .toLowerCase
+    .split("\\s+")
+    .map(prices)
+    .sum
+    .setScale(2)
+
+  println(s"Total cost of products is £$totalCost.")
 }
